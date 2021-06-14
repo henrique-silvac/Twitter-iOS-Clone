abd80bb895bd30a053c0c7b9e54575947f89e0aa
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-06-14 19:31:01 -0300

    Configure Project & Setup Root ViewController

---
+++ Twitter iOS Clone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  Twitter iOS Clone
+//
+//  Created by Henrique Silva on 14/06/21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
