01aae66410910c2e6b4365b656605b635709cbdd
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-06-18 17:26:43 -0300

    Getting Started With Login UI

---
+++ Twitter iOS Clone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  Twitter iOS Clone
+//
+//  Created by Henrique Silva on 18/06/21.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+        
+    }
+    
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+}
