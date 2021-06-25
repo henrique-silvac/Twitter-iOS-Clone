acaeb4959c49b0bffac49cda9d8955955c839341
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-06-25 15:14:56 -0300

    Upload User Data

--- Twitter iOS Clone/Controller/Authentication/RegistrationController.swift
+++ Twitter iOS Clone/Controller/Authentication/RegistrationController.swift
@@ -13,6 +13,7 @@ class RegistrationController: UIViewController {
     //MARK: - Properties
     
     private let imagePicker = UIImagePickerController()
+    private var profileImage: UIImage?
     
     private let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
@@ -102,15 +103,28 @@ class RegistrationController: UIViewController {
     }
     
     @objc func handleRegistration() {
+        guard let profileImage = profileImage else {
+            print("DEBUG: Please select a profile image...")
+            return
+        }
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
+        guard let fullname = fullnameTextField.text else { return }
+        guard let username = usernameTextField.text else { return }
         
         Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
             if let error = error {
                 print("DEBUG: Error is\(error.localizedDescription)")
                 return
             }
-            print("DEBUG: Succesffully registered user")
+            guard let uid = result?.user.uid else { return }
+            
+            let values = ["email": email, "username": username, "fullnamed": fullname]
+            
+            let  ref = Database.database().reference().child("user").child(uid)
+            ref.updateChildValues(values) { (error, ref) in
+                print("DEBUG: Succesffully update user information...")
+            }
         }
     }
     
@@ -150,6 +164,7 @@ extension RegistrationController: UIImagePickerControllerDelegate, UINavigationC
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         guard let profileImage = info[.editedImage] as? UIImage else { return }
+        self.profileImage = profileImage
         
         plusPhotoButton.layer.cornerRadius = 128/2
         plusPhotoButton.layer.masksToBounds = true
