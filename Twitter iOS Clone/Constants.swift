f564aeda55c38b5654315429a875fc73d62ea5a2
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-06-28 16:59:30 -0300

    Upload User Profile Image

---
+++ Twitter iOS Clone/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Twitter iOS Clone
+//
+//  Created by Henrique Silva on 28/06/21.
+//
+
+import Firebase
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+let DB_REF = Database.database().reference()
+let REF_USER = DB_REF.child("users")
--- Twitter iOS Clone/Controller/Authentication/RegistrationController.swift
+++ Twitter iOS Clone/Controller/Authentication/RegistrationController.swift
@@ -111,19 +111,28 @@ class RegistrationController: UIViewController {
         guard let password = passwordTextField.text else { return }
         guard let fullname = fullnameTextField.text else { return }
         guard let username = usernameTextField.text else { return }
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else {return}
         
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-            if let error = error {
-                print("DEBUG: Error is\(error.localizedDescription)")
-                return
-            }
-            guard let uid = result?.user.uid else { return }
-            
-            let values = ["email": email, "username": username, "fullnamed": fullname]
-            
-            let  ref = Database.database().reference().child("user").child(uid)
-            ref.updateChildValues(values) { (error, ref) in
-                print("DEBUG: Succesffully update user information...")
+        let filename = NSUUID().uuidString
+        let storageRef = STORAGE_PROFILE_IMAGES.child(filename)
+        
+        storageRef.putData(imageData, metadata: nil) { (meta, error) in
+            storageRef.downloadURL { (url, error) in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    if let error = error {
+                        print("DEBUG: Error is\(error.localizedDescription)")
+                        return
+                    }
+                    guard let uid = result?.user.uid else { return }
+                    
+                    let values = ["email": email, "username": username, "fullnamed": fullname]
+                    
+                    REF_USER.child(uid).updateChildValues(values) { (error, ref) in
+                        print("DEBUG: Succesffully update user information...")
+                    }
+                }
             }
         }
     }
